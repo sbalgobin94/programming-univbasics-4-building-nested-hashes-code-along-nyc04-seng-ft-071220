d1ecd8e6579f3728bc8d6b103fea937cbd5916f0
Samantha Balgobin <sbalgobin94@gmail.com>
2020-07-21 19:20:30 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -21,12 +21,19 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
         #your key/value pairs here
         },
       :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []

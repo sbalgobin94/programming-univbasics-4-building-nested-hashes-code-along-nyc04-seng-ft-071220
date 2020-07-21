f5e0627d26241cd448b36fc943968f8590d1bcec
Samantha Balgobin <sbalgobin94@gmail.com>
2020-07-21 19:18:41 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,12 +2,19 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
         #your key/value pairs here
         },
       :hero => {
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
